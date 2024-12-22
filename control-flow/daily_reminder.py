f3b6ab4bc0b66bf2b55b65914fa610e9db44c1dc
nattytefera <nhattysafari@gmail.com>
2024-12-22 23:52:44 +0300

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ")
-priority = input("priority (high/medium/low): ").lower()
-time_bound = input("is it time bound? (yes/no): ").lower()
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time bound? (yes/no): ").lower()
 
 match priority:
     case "high":
